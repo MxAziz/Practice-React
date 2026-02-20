886b0b69d788fb718f86ae0a70abe0c15b50c3ff
MxAziz <hereabdulaziz@gmail.com>
2026-02-20 12:50:36 +0600

    counter apps work successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
-  let count = 10;
+  const [counter, setCounter] = useState(0)
 
   const addValue = () => {
-    console.log('Add button clicked', Math.random())
+    console.log('Add button clicked', counter)
+    setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    console.log('remove button clicked', counter)
+    setCounter(counter - 1);
   }
 
   return (
     <>
       <h1>React counter App</h1>
-      <h3>Trigger Counter: {count} </h3>
+      <h3>Trigger Counter: {counter} </h3>
       <div style={{}}>
         <button onClick={addValue}>Add Value</button>
-        <button>Remove Value</button>
+        <button onClick={removeValue}>Remove Value</button>
       </div>
     </>
   );
